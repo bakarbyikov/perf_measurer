21f747d4fc4246bfcdd761fdd683bfec691b2eba
Andrey <bakarbyikov@gmail.com>
2023-03-11 16:48:17 +0500

    Added type hints

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
+from functools import wraps
 from time import perf_counter
-from typing import Callable
+from typing import Callable, ParamSpec, TypeVar
 
+
+P = ParamSpec("P")
+R = TypeVar("R")
 
 class count_time:
     counted_functions = list()
 
     @classmethod
     def print(cls) -> None:
+        name_width = max(map(lambda x: len(x.__qualname__), cls.counted_functions))
         for f in cls.counted_functions:
-            print((f"func={f.__module__}.{f.__name__}: "
-                   +f"elapsed={f.elapsed}, calls={f.calls}, "
-                   +f"avg={f.elapsed/max(1, f.calls)}"))
+            f:Callable
+            print((f"{f.__qualname__:<{name_width}}: "
+                   +f"elapsed={f.elapsed}, calls={f.calls:.2E}, "
+                   +f"avg={f.elapsed/max(1, f.calls):.2E}"))
 
     @classmethod
-    def __call__(cls, func: Callable) -> Callable:
+    def __call__(cls, func: Callable[P, R]) -> Callable[P, R]:
         cls.counted_functions.append(func)
         
-        def inner(*args, **kwds):
+        @wraps(func)
+        def inner(*args: P.args, **kwargs: P.kwargs) -> R:
             then = perf_counter()
-            res = func(*args, **kwds)
+            res = func(*args, **kwargs)
             func.elapsed += perf_counter() - then
             func.calls += 1
 
@@ -28,3 +35,4 @@ class count_time:
         func.calls = 0
 
         return inner
+
